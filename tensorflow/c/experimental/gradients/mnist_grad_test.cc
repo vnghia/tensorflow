3fe8f26ab3569f14d43cedcbb42e1120f011813a
Võ Văn Nghĩa <vovannghia2409@gmail.com>
2021-01-23 16:31:21 +0700

    initial setup for `TestMNISTTraining`: reading dataset

--- tensorflow/c/experimental/gradients/mnist_grad_test.cc
+++ tensorflow/c/experimental/gradients/mnist_grad_test.cc
@@ -9,6 +9,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
+#include <fstream>
+
 #include "tensorflow/c/eager/c_api_test_util.h"
 #include "tensorflow/c/eager/c_api_unified_experimental_internal.h"
 #include "tensorflow/c/eager/unified_api_testutil.h"
@@ -19,6 +21,7 @@ limitations under the License.
 #include "tensorflow/c/experimental/ops/math_ops.h"
 #include "tensorflow/c/experimental/ops/nn_ops.h"
 #include "tensorflow/c/tf_status_helper.h"
+#include "tensorflow/core/platform/path.h"
 #include "tensorflow/core/platform/tensor_float_32_utils.h"
 #include "tensorflow/core/platform/test.h"
 
@@ -116,6 +119,114 @@ Status MakeMNISTInput(AbstractContext* ctx,
   return Status::OK();
 }
 
+int ReverseInt(int i) {
+  unsigned char c1, c2, c3, c4;
+  c1 = (i & 255);
+  c2 = (i >> 8) & 255;
+  c3 = (i >> 16) & 255;
+  c4 = (i >> 24) & 255;
+  return (static_cast<int>(c1) << 24) + (static_cast<int>(c2) << 16) +
+         (static_cast<int>(c3) << 8) + c4;
+}
+
+void ReadMNISTHeader(std::ifstream* file, int* value) {
+  file->read(reinterpret_cast<char*>(value), sizeof(*value));
+  *value = ReverseInt(*value);
+}
+
+Status ReadMNISTImageData(AbstractContext* ctx, const std::string& path,
+                          std::vector<AbstractTensorHandle*>* images,
+                          int* image_size) {
+  std::ifstream file(path, std::ios::binary);
+  if (!file.is_open()) return Status(error::FAILED_PRECONDITION, path);
+
+  int magic_number = 0;
+  int number_of_images = 0;
+  int n_rows = 0;
+  int n_cols = 0;
+
+  ReadMNISTHeader(&file, &magic_number);
+  if (magic_number != 2051) {
+    return Status(error::INVALID_ARGUMENT, std::to_string(magic_number));
+  }
+  ReadMNISTHeader(&file, &number_of_images);
+  ReadMNISTHeader(&file, &n_rows);
+  ReadMNISTHeader(&file, &n_cols);
+
+  int image_s = n_cols * n_rows;
+  if (image_size) *image_size = image_s;
+
+  std::unique_ptr<unsigned char[]> image_data(new unsigned char[image_s]);
+  std::unique_ptr<float[]> tensor_data(new float[image_s]);
+  int64_t dims[] = {1, image_s};
+
+  images->resize(number_of_images);
+  for (int i = 0; i < number_of_images; i++) {
+    file.read(reinterpret_cast<char*>(image_data.get()), image_s);
+
+    for (int i{}; i < image_s; ++i) {
+      tensor_data[i] = image_data[i];
+    }
+    TF_RETURN_IF_ERROR(TestTensorHandleWithDimsFloat(ctx, tensor_data.get(),
+                                                     dims, 2, &((*images)[i])));
+  }
+
+  return Status::OK();
+}
+
+Status ReadMNISTLabelData(AbstractContext* ctx, const std::string& path,
+                          std::vector<AbstractTensorHandle*>* labels) {
+  std::ifstream file(path, std::ios::binary);
+  if (!file.is_open()) return Status(error::FAILED_PRECONDITION, path);
+
+  int magic_number = 0;
+  int number_of_labels = 0;
+
+  ReadMNISTHeader(&file, &magic_number);
+  if (magic_number != 2049) {
+    return Status(error::INVALID_ARGUMENT, std::to_string(magic_number));
+  }
+  ReadMNISTHeader(&file, &number_of_labels);
+
+  labels->resize(number_of_labels);
+  unsigned char label_data = 0;
+  int tensor_data = 0;
+  int64_t dims[] = {1};
+
+  for (int i = 0; i < number_of_labels; i++) {
+    file.read(reinterpret_cast<char*>(&label_data), 1);
+
+    tensor_data = label_data;
+    TF_RETURN_IF_ERROR(TestTensorHandleWithDimsInt(ctx, &tensor_data, dims, 1,
+                                                   &((*labels)[i])));
+  }
+
+  return Status::OK();
+}
+
+Status ReadMNISTData(AbstractContext* ctx, const std::string& dir,
+                     std::vector<AbstractTensorHandle*>* train_images,
+                     std::vector<AbstractTensorHandle*>* train_labels,
+                     std::vector<AbstractTensorHandle*>* test_images,
+                     std::vector<AbstractTensorHandle*>* test_labels,
+                     int* image_size) {
+  constexpr char train_images_path[] = "train-images-idx3-ubyte";
+  constexpr char train_labels_path[] = "train-labels-idx1-ubyte";
+  constexpr char test_images_path[] = "t10k-images-idx3-ubyte";
+  constexpr char test_labels_path[] = "t10k-labels-idx1-ubyte";
+
+  TF_RETURN_IF_ERROR(ReadMNISTImageData(
+      ctx, io::JoinPath(dir, train_images_path), train_images, image_size));
+  TF_RETURN_IF_ERROR(ReadMNISTLabelData(
+      ctx, io::JoinPath(dir, train_labels_path), train_labels));
+  TF_RETURN_IF_ERROR(ReadMNISTImageData(
+      ctx, io::JoinPath(dir, test_images_path), test_images, nullptr));
+  TF_RETURN_IF_ERROR(ReadMNISTLabelData(
+      ctx, io::JoinPath(dir, test_labels_path), test_labels));
+
+  return Status::OK();
+}
+
 class CppGradients
     : public ::testing::TestWithParam<std::tuple<const char*, bool, bool>> {
  protected:
@@ -178,6 +289,29 @@ TEST_P(CppGradients, TestMNISTGrad) {
                                            /*dims*/ {2, 2}, abs_error));
   UnrefTensorHandles(&mnist_inputs);
   UnrefTensorHandles(&outputs);
+}
+
+TEST_P(CppGradients, TestMNISTTraining) {
+  if (UseFunction()) {
+    // TODO(b/168850692): Enable this.
+    GTEST_SKIP() << "Can't take gradient of "
+                    "SparseSoftmaxCrossEntropyWithLogits in tracing mode.";
+  }
+
+  auto dataset_dir =
+      std::getenv("TF_CPP_GRADIENTS_MNIST_GRAD_TEST_DATASET_DIR");
+  if (!dataset_dir) GTEST_SKIP();
+
+  std::vector<AbstractTensorHandle*> train_images;
+  std::vector<AbstractTensorHandle*> train_labels;
+  std::vector<AbstractTensorHandle*> test_images;
+  std::vector<AbstractTensorHandle*> test_labels;
+  int image_size;
+
+  status_ =
+      ReadMNISTData(immediate_execution_ctx_.get(), dataset_dir, &train_images,
+                    &train_labels, &test_images, &test_labels, &image_size);
+  ASSERT_EQ(errors::OK, status_.code()) << status_.error_message();
 }
 
 #ifdef PLATFORM_GOOGLE
